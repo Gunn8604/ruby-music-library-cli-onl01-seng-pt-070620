2deb3db92fb000e8a37e71b592394177c935f73a
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-16 15:30:23 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,33 @@
+class Genre   
+  attr_accessor :name, :songs 
+  
+  @@all = []
+
+  def initialize(name)
+  @name = name
+  @songs = []
+  save    #saving all instances of the genre created
+  end
+
+  def self.all
+  @@all
+  end
+
+  def self.destroy_all
+  @@all = []
+  end
+
+  def save
+  @@all << self 
+  end
+
+  def self.create(genre)
+  self.new(genre)
+  end
+  
+  def songs 
+    @songs
+  end 
+  
+
+end     #class end
---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song 
+  attr_accessor :name, :genre
+  
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil) #artist=nil makes it an optional argument
+  @name = name
+    if artist != nil 
+      self.artist = artist
+    end
+    
+    if genre != nil 
+      self.genre = genre
+    end 
+  save    #saving all instances of the song created
+  end
+  
+  def artist 
+    @artist
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)     #invokes Artist#add_song to add itself to the artist's collection of songs(artist has many songs)
+  end
+
+def genre 
+    @genre 
+  end
+  
+  def genre=(genre)
+    @genre = genre
+
+    #genre.add_song(self)     
+  end
+
+  def self.all
+  @@all
+  end
+
+  def self.destroy_all
+  @@all = []
+  end
+
+  def save
+  @@all << self 
+  end
+
+  def self.create(name)
+  self.new(name)
+  end
+
+end     #class end
