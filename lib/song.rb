5d2b041dd63ac19f47c39f767ed679635f5602c8
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-16 20:46:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ def genre
   def genre=(genre)
     @genre = genre
     Genre.all << self
-    genre.songs << self unless genre.songs.include?(self)
+    genre.songs << self unless genre.songs.include?(self)   #adds the song to the genre's collection of songs (genre has many songs)
     #Genre.add_song(self)     
   end
 
